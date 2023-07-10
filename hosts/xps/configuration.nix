a17d53e63e9b8f88dfba87ebe671be41184882c4
Francisco Dibar <frandibar@gmail.com>
2023-07-10 20:52:05 -0300

    Improve font scaling

--- hosts/xps/configuration.nix
+++ hosts/xps/configuration.nix
@@ -10,4 +10,12 @@
   # Enable touchpad support
   services.xserver.libinput.enable = true;
 
+  # HiDPI scale fonts
+  # Extracted from https://nixos.wiki/wiki/Xorg
+  services.xserver.dpi = 180;
+  environment.variables = {
+    GDK_SCALE = "2";
+    GDK_DPI_SCALE = "0.5";
+    _JAVA_OPTIONS = "-Dsun.java2d.uiScale=2";
+  };
 }
