1c536e08dca8ee4a88bff203c99e1f239afb4298
Francisco Dibar <frandibar@gmail.com>
2024-08-26 16:03:24 -0300

    add fjd_mv-facturas

--- sources/emacs/lisp/fjd.el
+++ sources/emacs/lisp/fjd.el
@@ -23,5 +23,74 @@
     (yank)))
 
 
+(defvar fjd_facturas-dir (expand-file-name "~/Sync/keybase/docs/facturas/"))
+
+
+(defun fjd_mv-facturas ()
+  "Move facturas from ~/Downloads to their corresponding directory.
+
+This function could be a shell script but I prefer living in Emacs."
+  (interactive)
+  (let* ((downloads-dir (expand-file-name "~/Downloads/"))
+	 (servicios '("aysa"
+		      "edenor"
+		      "naturgy"
+		      "osde"
+		      "personal"))
+	 (downloaded-files (directory-files downloads-dir
+					    nil ; relative names
+					    ".pdf$")))
+    (mapc (lambda (file)
+	    (let ((filename (file-name-sans-extension file)))
+	      (when (member filename servicios)
+		(let ((cmd (concat "mv "
+				   downloads-dir
+				   file
+				   " "
+				   fjd_facturas-dir
+				   filename
+				   "/"
+				   (fjd_mv-facturas--next-file-name filename))))
+		  (message cmd)
+		  (call-process-shell-command cmd)))))
+	  downloaded-files)))
+
+
+(defun fjd_mv-facturas--yy-mm-to-filename (yy-mm)
+  "Given YY-MM as (yy mm), return string 'YY-MM.pdf'."
+  (apply 'format "%02d-%02d.pdf" yy-mm))
+
+
+(defun fjd_mv-facturas--filename-to-yy-mm (filename)
+  "Parse FILENAME 'YY-MM.pdf' into (YY MM)."
+  (mapcar #'string-to-number
+	  (s-split "-"
+		   (car
+		    (s-split "\\."
+			     filename)))))
+
+
+(defun fjd_mv-facturas--next-period (yy-mm)
+  "Given YY-MM as (yy mm), add one month and return result."
+  (pcase-let ((`(,yy ,mm) yy-mm))
+    (if (= mm 12)
+	(list (1+ yy) 1)
+      (list yy (1+ mm)))))
+
+
+(defun fjd_mv-facturas--next-file-name (service)
+  "Return the filename of the next period for SERVICE."
+  (let* ((files (directory-files
+		 (concat fjd_facturas-dir service)
+		 nil			; relative name
+		 "[0-9][0-9]-[0-9][0-9].pdf"))
+	 (last-period-filename (car (last files))))
+    (-> last-period-filename
+	fjd_mv-facturas--filename-to-yy-mm
+	fjd_mv-facturas--next-period
+	fjd_mv-facturas--yy-mm-to-filename)))
+
+
+
 (provide 'fjd)
 ;;; fjd.el ends here
