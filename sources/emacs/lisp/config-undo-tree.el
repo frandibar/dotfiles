ef5b338c46c2bf485eb7143435b8cc41a844266c
Francisco Dibar <frandibar@gmail.com>
2024-08-26 16:00:10 -0300

    replace undo-tree with vundo

--- sources/emacs/lisp/config-undo-tree.el
+++ sources/emacs/lisp/config-undo-tree.el
@@ -2,14 +2,22 @@
 ;;; Commentary:
 ;;; Code:
 
-(use-package undo-tree
-  :functions global-undo-tree-mode
+;; vundo is a newer replacement for undo-tree, it uses less space,
+;; provides a diff and looks prettier.
+(use-package vundo
   :custom
-  ;; Avoid cluttering the filesystem with ~undo-tree~ files.
-  (undo-tree-auto-save-history nil)
+  (vundo-glyph-alist vundo-unicode-symbols))
 
-  :config
-  (global-undo-tree-mode 1))
+
+;; (use-package undo-tree
+;;   :functions global-undo-tree-mode
+;;   :custom
+;;   ;; Avoid cluttering the filesystem with ~undo-tree~ files.
+;;   (undo-tree-auto-save-history nil)
+
+;;   :config
+;;   (global-undo-tree-mode 1))
+
 
 (provide 'undo-tree)
 ;;; config-undo-tree.el ends here
